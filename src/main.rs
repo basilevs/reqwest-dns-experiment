05f623424b1a0500f7c2e139e3f538001f6b5a90
Vasili Gulevich <vasili.gulevich@xored.com>
2024-12-08 21:33:52 +0400

    Compiles, DNS assertion fails

---
+++ src/main.rs
@@ -0,0 +1,38 @@
+use reqwest::blocking::ClientBuilder;
+use anyhow::{Result, Error as AnyError};
+use reqwest::Error as ReqError;
+use std::error::Error as StdError;
+
+fn main() {
+    
+    assert!(!is_transient_result(&ping("https://httpbin.spirenteng.com/meh")), "HTTP 404 is expected");
+    assert!(!is_transient_result(&ping("https://httpbin.spirenteng.com/get")), "HTTP 200 is expected");
+    assert!(is_transient_result(&ping("https://unknown_host1.com/meh")), "DNS failure is expected");
+    assert!(is_transient_result(&ping("https://240.0.0.1/meh")), "Connection failure is expected");
+}
+
+fn is_transient_result(result: &Result<String>) -> bool {
+    return match  result {
+        Ok(_) => false,
+        Err(e) => is_transient_error(e.as_ref()), 
+    }
+}
+
+fn is_transient_error(e1: &(dyn std::error::Error + 'static)) -> bool {
+    if let Some(&e3) = e1.downcast_ref::<&ReqError>() {
+        if let Some(source) =  e3.source() {
+            return is_transient_error(&source);
+        }
+    }
+
+    if let Some(e5) = e1.source() {
+        print!("{}: {}\n",std::any::type_name_of_val(e5), &e5);
+        return is_transient_error(e5)
+    }
+    
+    return false
+}
+
+fn ping(url: &str) -> Result<String> {
+    return Ok(ClientBuilder::default().build()?.get(url).send()?.text()?);
+}
